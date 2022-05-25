5926cfbb64253e0baefdbc3b42fd1fd11ffaeaff
Tiago Felix Prudente <tprudente@server01.foursys.local>
2022-05-25 14:40:55 -0300

    Created Transaction Data

--- FourStoreSolo/src/br/com/foursys/fourcamp/fourstore/data/TransactionData.java
+++ FourStoreSolo/src/br/com/foursys/fourcamp/fourstore/data/TransactionData.java
@@ -1,5 +1,27 @@
 package br.com.foursys.fourcamp.fourstore.data;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import br.com.foursys.fourcamp.fourstore.model.Transaction;
+
 public class TransactionData {
+
+	private static List<Transaction> transactionList = new ArrayList<Transaction>();
+
+	public boolean save(Transaction transaction) {
+		for (Transaction item : transactionList) {
+			if (!item.getIdTransaction().equals(transaction.getIdTransaction())) {
+				transactionList.add(transaction);
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	public List<Transaction> listAll() {
+		return transactionList;
+	}
 
 }
